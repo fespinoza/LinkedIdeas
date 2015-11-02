c6342ca9c6e629c6ca120e869394579bec4656c4
Felipe Espinoza <fespinozacast@gmail.com>
2015-11-02 18:07:54 +0100

    Initial Commit

---
+++ LinkedIdeas/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LinkedIdeas
+//
+//  Created by Felipe Espinoza Castillo on 02/11/15.
+//  Copyright © 2015 Felipe Espinoza Dev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
