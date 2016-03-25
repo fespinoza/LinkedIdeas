6bdf3eb1bd84289f1f280031818453621905b861
Felipe Espinoza <fespinozacast@gmail.com>
2016-03-25 23:34:38 +0100

    Remove commented out code

--- LinkedIdeas/AppDelegate.swift
+++ LinkedIdeas/AppDelegate.swift
@@ -10,13 +10,13 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-  
+
   func applicationDidFinishLaunching(aNotification: NSNotification) {
     // Insert code here to initialize your application
   }
-  
+
   func applicationWillTerminate(aNotification: NSNotification) {
     // Insert code here to tear down your application
   }
-  
+
 }
--- LinkedIdeas/Document.swift
+++ LinkedIdeas/Document.swift
@@ -18,16 +18,16 @@ class DocumentData: NSObject, NSCoding {
   var readLinks: [Link]?
   var writeConcepts: [Concept]?
   var writeLinks: [Link]?
-  
+
   override init() {
     super.init()
   }
-  
+
   required init?(coder aDecoder: NSCoder) {
     readConcepts = aDecoder.decodeObjectForKey("concepts") as! [Concept]?
     readLinks = aDecoder.decodeObjectForKey("links") as! [Link]?
   }
-  
+
   func encodeWithCoder(aCoder: NSCoder) {
     aCoder.encodeObject(writeConcepts, forKey: "concepts")
     aCoder.encodeObject(writeLinks, forKey: "links")
@@ -35,20 +35,20 @@ class DocumentData: NSObject, NSCoding {
 }
 
 class Document: NSDocument {
-  
+
   @IBOutlet weak var canvas: CanvasView!
   @IBOutlet weak var conceptMode: NSButton!
   @IBOutlet weak var linkMode: NSButton!
-  
+
   @IBOutlet var ultraWindow: NSWindow!
   var documentData = DocumentData()
   var editionMode = Mode.Concepts
-  
+
   override init() {
     super.init()
     // Add your subclass-specific initialization here.
   }
-  
+
   override func windowControllerDidLoadNib(aController: NSWindowController) {
     super.windowControllerDidLoadNib(aController)
     // Add any code here that needs to be executed once the windowController has loaded the document's window.
@@ -57,17 +57,17 @@ class Document: NSDocument {
     //    canvas.mode = editionMode
     ultraWindow.acceptsMouseMovedEvents = true
   }
-  
+
   override class func autosavesInPlace() -> Bool {
     return true
   }
-  
+
   override var windowNibName: String? {
     // Returns the nib file name of the document
     // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
     return "Document"
   }
-  
+
   override func dataOfType(typeName: String) throws -> NSData {
     // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
     // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
@@ -75,14 +75,14 @@ class Document: NSDocument {
     //    documentData.writeLinks = canvas.links
     return NSKeyedArchiver.archivedDataWithRootObject(documentData)
   }
-  
+
   override func readFromData(data: NSData, ofType typeName: String) throws {
     // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
     // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
     // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
     documentData = NSKeyedUnarchiver.unarchiveObjectWithData(data) as! DocumentData
   }
-  
+
   @IBAction func changeMode(sender: NSButton) {
     if sender == conceptMode {
       editionMode = .Concepts
