aa8faff59b0055ec82850e135a46d21d47509b79
Felipe Espinoza <fespinozacast@gmail.com>
2016-03-26 16:13:30 +0100

    Add Link Model

--- LinkedIdeas/Link.swift
+++ LinkedIdeas/Link.swift
@@ -8,40 +8,41 @@
 
 import Foundation
 
-class Link: NSObject, NSCoding {
+class Link: NSObject, NSCoding, Element {
+  // own attributes
   var origin: Concept
   var target: Concept
-  var stringValue: String = ""
   var originPoint: NSPoint { return origin.point }
   var targetPoint: NSPoint { return target.point }
-  var rect: NSRect { return NSRect(p1: originPoint, p2: targetPoint) }
-  var added: Bool
-  var editing: Bool
+  
   override var description: String {
-    return "'\(origin.stringValue)' \(stringValue) '\(target.stringValue)'"
+    return "'\(origin.stringValue)' '\(target.stringValue)'"
+  }
+  
+  // Element
+  var identifier: String
+  var minimalRect: NSRect {
+    return NSRect(p1: originPoint, p2: targetPoint)
   }
   
   init(origin: Concept, target: Concept) {
     self.origin = origin
     self.target = target
-    self.added  = false
-    self.editing = false
+    self.identifier = "\(random())-link"
   }
   
-  let stringValueKey = "StringValueKey"
+  let identifierKey = "identifierKey"
   let originKey = "OriginKey"
   let targetKey = "TargetKey"
   
   required init?(coder aDecoder: NSCoder) {
-    stringValue = aDecoder.decodeObjectForKey(stringValueKey) as! String
+    identifier = aDecoder.decodeObjectForKey(identifierKey) as! String
     origin = aDecoder.decodeObjectForKey(originKey) as! Concept
     target = aDecoder.decodeObjectForKey(targetKey) as! Concept
-    added = false
-    editing = false
   }
   
   func encodeWithCoder(aCoder: NSCoder) {
-    aCoder.encodeObject(stringValue, forKey: stringValueKey)
+    aCoder.encodeObject(identifier, forKey: identifierKey)
     aCoder.encodeObject(origin, forKey: originKey)
     aCoder.encodeObject(target, forKey: targetKey)
   }
---
+++ LinkedIdeasTests/LinkTests.swift
@@ -0,0 +1,14 @@
+//
+//  LinkTests.swift
+//  LinkedIdeas
+//
+//  Created by Felipe Espinoza Castillo on 26/03/16.
+//  Copyright © 2016 Felipe Espinoza Dev. All rights reserved.
+//
+
+import XCTest
+@testable import LinkedIdeas
+
+class LinkTests: XCTestCase {
+  
+}
