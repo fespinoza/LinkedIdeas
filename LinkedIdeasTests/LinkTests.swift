7f5d73f63f6e7d91600f9c4bc701df734902e93d
Felipe Espinoza <fespinozacast@gmail.com>
2016-03-27 19:34:31 +0200

    Add testForMinimalRect

--- LinkedIdeasTests/LinkTests.swift
+++ LinkedIdeasTests/LinkTests.swift
@@ -11,4 +11,16 @@ import XCTest
 
 class LinkTests: XCTestCase {
   
+  func testMinimalRect() {
+    // given
+    let concept1 = Concept(point: NSMakePoint(20, 30))
+    let concept2 = Concept(point: NSMakePoint(120, 130))
+    
+    // when
+    let link = Link(origin: concept1, target: concept2)
+    
+    // then
+    XCTAssertEqual(link.minimalRect, NSMakeRect(20, 30, 100, 100))
+  }
+  
 }
