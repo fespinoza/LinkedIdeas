492752516f81d0a91b96a3a3959fce805806fc98
Felipe Espinoza <fespinozacast@gmail.com>
2016-08-23 19:36:39 +0200

    Remove hockeyapp.

--- LinkedIdeas/AppDelegate.swift
+++ LinkedIdeas/AppDelegate.swift
@@ -7,17 +7,12 @@
 //
 
 import Cocoa
-import HockeySDK
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     // Insert code here to initialize your application
-    if let sharedHockeyManager = BITHockeyManager.shared() {
-      sharedHockeyManager.configure(withIdentifier: "910181e274534fc1a9c01ad131c17ea8")
-      sharedHockeyManager.start()
-    }
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
