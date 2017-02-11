fb6ab7a9d60d053e5244a20576653c0749783211
Felipe Espinoza <fespinozacast@gmail.com>
2017-02-11 01:14:52 +0100

    Use arrow points as calculated privated variables.

--- LinkedIdeas/Arrow.swift
+++ LinkedIdeas/Arrow.swift
@@ -13,25 +13,41 @@ struct Arrow {
   var p2: NSPoint
   let arrowHeight: CGFloat = 20
   let arrowWidth: CGFloat = 15
-  let arrowBodyWidth: CGFloat = 5
+  let arrowBodyWidth: CGFloat
+  
+  init(p1: NSPoint, p2: NSPoint) {
+    self.p1 = p1
+    self.p2 = p2
+    self.arrowBodyWidth = 5
+  }
+  
+  init(p1: NSPoint, p2: NSPoint, arrowBodyWidth: CGFloat = 5) {
+    self.p1 = p1
+    self.p2 = p2
+    self.arrowBodyWidth = arrowBodyWidth
+  }
+  
+  private var m: CGFloat { return (p2.y - p1.y) / (p2.x - p1.x) }
+  private var C: CGFloat { return arrowBodyWidth / 2 }
+  private var B: CGFloat { return arrowWidth / 2 }
+  private var alpha: CGFloat { return atan(m) }
+  private var direction: CGFloat { return p2.x >= p1.x ? 1 : -1 }
+  
+  private var p5: NSPoint {
+    return NSMakePoint((p2.x - direction * cos(alpha) * arrowHeight), (p2.y - direction * sin(alpha) * arrowHeight))
+  }
+  private var p3: NSPoint { return NSMakePoint((p5.x - B * sin(alpha)), (p5.y + B * cos(alpha))) }
+  private var p4: NSPoint { return NSMakePoint((p5.x + B * sin(alpha)), (p5.y - B * cos(alpha))) }
+  private var p6: NSPoint { return NSMakePoint((p5.x - C * sin(alpha)), (p5.y + C * cos(alpha))) }
+  private var p7: NSPoint { return NSMakePoint((p5.x + C * sin(alpha)), (p5.y - C * cos(alpha))) }
+  private var p8: NSPoint { return NSMakePoint((p1.x - C * sin(alpha)), (p1.y + C * cos(alpha))) }
+  private var p9: NSPoint { return NSMakePoint((p1.x + C * sin(alpha)), (p1.y - C * cos(alpha))) }
+  
+  func arrowBodyPoints() -> [NSPoint] {
+    return [p6, p7, p8, p9]
+  }
 
   func bezierPath() -> NSBezierPath {
-    let B: CGFloat = arrowWidth / 2
-    let C: CGFloat = arrowBodyWidth / 2
-    let m = (p2.y - p1.y) / (p2.x - p1.x)
-    let alpha = atan(m)
-    let direction: CGFloat = p2.x >= p1.x ? 1 : -1
-
-    let p5 = NSMakePoint((p2.x - direction * cos(alpha) * arrowHeight), (p2.y - direction * sin(alpha) * arrowHeight))
-    let p3 = NSMakePoint((p5.x - B * sin(alpha)), (p5.y + B * cos(alpha)))
-    let p4 = NSMakePoint((p5.x + B * sin(alpha)), (p5.y - B * cos(alpha)))
-
-    // Arrow body
-    let p6 = NSMakePoint((p5.x - C * sin(alpha)), (p5.y + C * cos(alpha)))
-    let p7 = NSMakePoint((p5.x + C * sin(alpha)), (p5.y - C * cos(alpha)))
-    let p8 = NSMakePoint((p1.x - C * sin(alpha)), (p1.y + C * cos(alpha)))
-    let p9 = NSMakePoint((p1.x + C * sin(alpha)), (p1.y - C * cos(alpha)))
-
     let arrowPath = NSBezierPath()
     arrowPath.move(to: p1)
     arrowPath.line(to: p8)
