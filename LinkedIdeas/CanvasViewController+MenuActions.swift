5f185d594e6f56341961f221047ddc46c7e23d0e
Felipe Espinoza <fespinozacast@gmail.com>
2017-05-22 18:33:32 +0200

    Improve selection of colors for quick colors for concepts. (#44)
    * Improve selection of colors for quick colors for concepts.
    
    * Increase font size step when augmenting for diminshing size.

--- LinkedIdeas/CanvasViewController+MenuActions.swift
+++ LinkedIdeas/CanvasViewController+MenuActions.swift
@@ -8,6 +8,21 @@
 
 import Cocoa
 
+extension NSColor {
+  convenience init(fullRed: CGFloat, fullGreen: CGFloat, fullBlue: CGFloat) {
+    func normalizeValue(colorValue: CGFloat) -> CGFloat {
+      return colorValue / 255
+    }
+
+    self.init(
+      red: normalizeValue(colorValue: fullRed),
+      green: normalizeValue(colorValue: fullGreen),
+      blue: normalizeValue(colorValue: fullBlue),
+      alpha: 1.0
+    )
+  }
+}
+
 extension CanvasViewController {
 
   override func selectAll(_ sender: Any?) {
@@ -37,10 +52,12 @@ extension CanvasViewController {
 
   struct DefaultColors {
     static let color1 = NSColor.black
-    static let color2 = NSColor.red
-    static let color3 = NSColor.blue
-    static let color4 = NSColor.green
-    static let color5 = NSColor.purple
+    static let color2 = NSColor(fullRed: 0, fullGreen: 75, fullBlue: 213)
+    static let color3 = NSColor(fullRed: 226, fullGreen: 6, fullBlue: 6)
+    static let color4 = NSColor(fullRed: 83, fullGreen: 153, fullBlue: 6)
+    static let color5 = NSColor(fullRed: 162, fullGreen: 8, fullBlue: 174)
+    static let color6 = NSColor(fullRed: 255, fullGreen: 95, fullBlue: 0)
+    static let color7 = NSColor(fullRed: 73, fullGreen: 9, fullBlue: 152)
   }
 
   override func changeColor(_ sender: Any?) {
@@ -59,6 +76,10 @@ extension CanvasViewController {
       setColorForSelectedElements(fontColor: DefaultColors.color4)
     case "Color #5":
       setColorForSelectedElements(fontColor: DefaultColors.color5)
+    case "Color #6":
+      setColorForSelectedElements(fontColor: DefaultColors.color6)
+    case "Color #7":
+      setColorForSelectedElements(fontColor: DefaultColors.color7)
     default:
       break
     }
@@ -102,7 +123,7 @@ extension CanvasViewController {
     for concept in currentSelectedConcepts() {
       concept.attributedStringValue = NSAttributedString(
         attributedString: concept.attributedStringValue,
-        fontSize: concept.attributedStringValue.fontSize + 1
+        fontSize: concept.attributedStringValue.fontSize + 3
       )
     }
   }
@@ -111,7 +132,7 @@ extension CanvasViewController {
     for concept in currentSelectedConcepts() {
       concept.attributedStringValue = NSAttributedString(
         attributedString: concept.attributedStringValue,
-        fontSize: concept.attributedStringValue.fontSize - 1
+        fontSize: concept.attributedStringValue.fontSize - 3
       )
     }
   }
