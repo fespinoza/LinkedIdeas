4e01fca4bda63920163678de23ba4d5246d1b6fd
Felipe Espinoza <fespinozacast@gmail.com>
2017-05-29 18:16:43 +0200

    Extract implementation details from mouse* methods.
    so then there are custom helper functions that make the necessary
    actions after detecting a particular kind of event.

--- LinkedIdeas/CanvasViewController+MouseEvents.swift
+++ LinkedIdeas/CanvasViewController+MouseEvents.swift
@@ -11,6 +11,7 @@ import Cocoa
 // MARK: - CanvasViewController+MouseEvents
 
 extension CanvasViewController {
+  // MARK: - Mouse Events
   override func mouseDown(with event: NSEvent) {
     let point = convertToCanvasCoordinates(point: event.locationInWindow)
 
@@ -18,21 +19,21 @@ extension CanvasViewController {
       if let clickedElements = clickedElements(atPoint: point) {
         switch currentState {
         case .selectedElement(let selectedElement):
-          if !event.modifierFlags.contains(.shift) || (selectedElement as? Link) != nil {
+          if !isPressingShift(event: event) || (selectedElement as? Link) != nil {
             // single click, select another element
             safeTransiton {
               try stateManager.toSelectedElement(element: clickedElements.first!)
             }
           }
-          // else: it can be additional selection or link creation
+        // else: it can be additional selection or link creation
         case .multipleSelectedElements:
           // do nothing, because it's the init of multiple drag
           break
         default:
-            // by default clicking (or shift clicking) an element selects it.
-            safeTransiton {
-              try stateManager.toSelectedElement(element: clickedElements.first!)
-            }
+          // by default clicking (or shift clicking) an element selects it.
+          safeTransiton {
+            try stateManager.toSelectedElement(element: clickedElements.first!)
+          }
         }
 
       } else {
@@ -83,7 +84,8 @@ extension CanvasViewController {
       guard let concepts = elements as? [Concept] else {
         return
       }
-      if !event.modifierFlags.contains(.shift) {
+
+      if !isPressingShift(event: event) {
         drag(concepts: concepts, toPoint: point)
       }
 
@@ -108,40 +110,23 @@ extension CanvasViewController {
 
       if didDragStart() {
         if isDragShiftEvent(event) {
-          if let targetConcept = clickedSingleConcept(atPoint: point) {
-            targetConcept.isSelected = false
-
-            let link = saveLink(fromConcept: concept, toConcept: targetConcept)
-            dismissConstructionArrow()
-            safeTransiton {
-              try stateManager.toSelectedElement(element: link)
-            }
-          } else {
-            safeTransiton { try stateManager.toCanvasWaiting() }
-          }
+          whenClickedOnSingleConcept(atPoint: point, thenDo: { (targetConcept) in
+            createNewLinkUp(fromConcept: concept, toConcept: targetConcept)
+          })
         } else {
           endDrag(forConcept: concept, toPoint: point)
         }
       } else {
         // click
-        if let targetConcept = clickedSingleConcept(atPoint: point) {
+        whenClickedOnSingleConcept(atPoint: point, thenDo: { (targetConcept) in
           // shift + click on concept
-          if event.modifierFlags.contains(.shift) {
-            let elements = [element, targetConcept]
-            if (element as? Concept) != targetConcept {
-              safeTransiton {
-                try stateManager.toMultipleSelectedElements(elements: elements)
-              }
-            } else {
-              safeTransiton { try stateManager.toCanvasWaiting() }
-            }
+          if isPressingShift(event: event) {
+            shiftClickUp(onConcept: targetConcept, withSelectedElement: element)
           }
           // else: just single click on concept, do nothing
-        } else {
-          safeTransiton { try stateManager.toCanvasWaiting() }
-        }
+        })
       }
-    case .multipleSelectedElements(var elements):
+    case .multipleSelectedElements(let elements):
       guard let concepts = elements as? [Concept] else {
         return
       }
@@ -150,36 +135,9 @@ extension CanvasViewController {
         endDrag(forConcepts: concepts, toPoint: point)
       } else {
         // shift+click
-        if let targetConcept = clickedSingleConcept(atPoint: point) {
-          let elementAlreadySelected = elements.contains(where: { (element) -> Bool in
-            return (element as? Concept) == targetConcept
-          })
-
-          if elementAlreadySelected {
-            // unselect from group
-            elements.remove(at: elements.index(where: { (element) -> Bool in
-              return (element as? Concept) == targetConcept
-            })!)
-
-            if elements.count > 1 {
-              safeTransiton {
-                try stateManager.toMultipleSelectedElements(elements: elements)
-              }
-            } else {
-              safeTransiton {
-                try stateManager.toSelectedElement(element: elements.first!)
-              }
-            }
-          } else {
-            elements.append(targetConcept)
-
-            safeTransiton {
-              try stateManager.toMultipleSelectedElements(elements: elements)
-            }
-          }
-        } else {
-          safeTransiton { try stateManager.toCanvasWaiting() }
-        }
+        whenClickedOnSingleConcept(atPoint: point, thenDo: { (targetConcept) in
+          shiftClickUp(onConcept: targetConcept, withSelectedElements: elements)
+        })
       }
     case .canvasWaiting:
       selectHoveredConcepts()
@@ -189,5 +147,73 @@ extension CanvasViewController {
     }
 
     resetDraggingConcepts()
+  }
+
+  // MARK: - mouse action helpers
+
+  private func isPressingShift(event: NSEvent) -> Bool {
+    return event.modifierFlags.contains(.shift)
+  }
+
+  private func createNewLinkUp(fromConcept: Concept, toConcept: Concept) {
+    toConcept.isSelected = false
+
+    let link = saveLink(fromConcept: fromConcept, toConcept: toConcept)
+    dismissConstructionArrow()
+    safeTransiton {
+      try stateManager.toSelectedElement(element: link)
+    }
+  }
+
+  private func shiftClickUp(onConcept targetConcept: Concept, withSelectedElements elements: [Element]) {
+    guard var concepts = elements as? [Concept] else {
+      return
+    }
+
+    if concepts.contains(targetConcept) {
+      // unselect from group
+      concepts.remove(at: concepts.index(of: targetConcept)!)
+
+      if concepts.count > 1 {
+        safeTransiton {
+          try stateManager.toMultipleSelectedElements(elements: concepts)
+        }
+      } else {
+        safeTransiton {
+          try stateManager.toSelectedElement(element: concepts.first!)
+        }
+      }
+    } else {
+      concepts.append(targetConcept)
+
+      safeTransiton {
+        try stateManager.toMultipleSelectedElements(elements: concepts)
+      }
+    }
+  }
+
+  private func shiftClickUp(onConcept targetConcept: Concept, withSelectedElement element: Element) {
+    let elements = [element, targetConcept]
+    if (element as? Concept) != targetConcept {
+      safeTransiton {
+        try stateManager.toMultipleSelectedElements(elements: elements)
+      }
+    } else {
+      safeTransiton { try stateManager.toCanvasWaiting() }
+    }
+  }
+
+  /// handles the common case of evaluating if the clicked point matches a single concept then do some logic, otherwise
+  /// it will transition to .canvasWaiting
+  ///
+  /// - Parameters:
+  ///   - point: point from the click event
+  ///   - thenDo: what actions you want to perform when there was actually a clicked concept
+  private func whenClickedOnSingleConcept(atPoint point: NSPoint, thenDo: (Concept) -> Void) {
+    if let targetConcept = clickedSingleConcept(atPoint: point) {
+      thenDo(targetConcept)
+    } else {
+      safeTransiton { try stateManager.toCanvasWaiting() }
+    }
   }
 }
