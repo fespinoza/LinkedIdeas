fespinoza/LinkedIdeas

commit 09922f7ef039f92a9f4732d661e6fd03c5daa7c4

Swift 4 migration. (#70)
* Convert files to swift 4.

* Make `StateManager` a `class` instead of a `struct`.

I did not needed the value semantic in this “object” as one is being
references in the `CanvasViewController`.

I was also getting build errors because of
https://github.com/apple/swift-evolution/blob/master/proposals/0176-enfo
rce-exclusive-access-to-memory.md

So as a quick solution i just turned the struct into a class, the real
value type needed is the currentState value, which in this case is an
Enum.

* Update Readme.

LinkedIdeas/CanvasViewController+NSPasteboard.swift
@@ -83,7 +83,7 @@ extension CanvasViewController {
   // MARK: - Interface actions
 
   func cut(_ sender: Any?) {
-    writeToPasteboard(pasteboard: NSPasteboard.general())
+    writeToPasteboard(pasteboard: NSPasteboard.general)
     if let elements = selectedElements() {
       safeTransiton {
         try stateManager.toCanvasWaiting(deletingElements: elements)
@@ -92,14 +92,14 @@ extension CanvasViewController {
   }
 
   func copy(_ sender: Any?) {
-    writeToPasteboard(pasteboard: NSPasteboard.general())
+    writeToPasteboard(pasteboard: NSPasteboard.general)
   }
 
   func paste(_ sender: Any?) {
-    readFromPasteboard(pasteboard: NSPasteboard.general())
+    readFromPasteboard(pasteboard: NSPasteboard.general)
   }
 
   func pasteAsPlainText(_ sender: Any?) {
-    readFromPasteboardAsPlainText(pasteboard: NSPasteboard.general())
+    readFromPasteboardAsPlainText(pasteboard: NSPasteboard.general)
   }
 }

LinkedIdeas.xcodeproj/project.pbxproj
@@ -212,6 +212,9 @@
 		9BDDE7381BEF9157000AAD44 /* Concept.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = Concept.swift; sourceTree = "<group>"; };
 		9BE40C201D8DD98B0025F154 /* CanvasViewControllerTests.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = CanvasViewControllerTests.swift; sourceTree = "<group>"; };
 		9BF15C381CAC69A500C95041 /* WindowController.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = WindowController.swift; sourceTree = "<group>"; };
+		9BF456E21F16E8B800E1CDD3 /* LICENSE */ = {isa = PBXFileReference; lastKnownFileType = text; path = LICENSE; sourceTree = "<group>"; };
+		9BF456E31F16E8BC00E1CDD3 /* README.md */ = {isa = PBXFileReference; lastKnownFileType = net.daringfireball.markdown; path = README.md; sourceTree = "<group>"; };
+		9BF456E41F16E8BF00E1CDD3 /* PR.md */ = {isa = PBXFileReference; lastKnownFileType = net.daringfireball.markdown; path = PR.md; sourceTree = "<group>"; };
 /* End PBXFileReference section */
 
 /* Begin PBXFrameworksBuildPhase section */
@@ -243,6 +246,9 @@
 			isa = PBXGroup;
 			children = (
 				9B66961B1D08AF9F00CDE806 /* linked_ideas_doc_icon.icns */,
+				9BF456E21F16E8B800E1CDD3 /* LICENSE */,
+				9BF456E41F16E8BF00E1CDD3 /* PR.md */,
+				9BF456E31F16E8BC00E1CDD3 /* README.md */,
 				9B2F1D9E1BE7CFE70090CC18 /* LinkedIdeas */,
 				9B2F1DB31BE7CFE80090CC18 /* LinkedIdeasTests */,
 				9BD850B11EE4C199005B89C9 /* QLLinkedIdeas */,
@@ -516,17 +522,17 @@
 					9B2F1D9B1BE7CFE70090CC18 = {
 						CreatedOnToolsVersion = 7.1;
 						DevelopmentTeam = 4E6CZX3H5W;
-						LastSwiftMigration = 0820;
+						LastSwiftMigration = 0900;
 					};
 					9B2F1DAF1BE7CFE80090CC18 = {
 						CreatedOnToolsVersion = 7.1;
-						LastSwiftMigration = 0820;
+						LastSwiftMigration = 0900;
 						TestTargetID = 9B2F1D9B1BE7CFE70090CC18;
 					};
 					9BD850AF1EE4C199005B89C9 = {
 						CreatedOnToolsVersion = 8.2.1;
 						DevelopmentTeam = 2Z7R9FQBNN;
-						LastSwiftMigration = 0820;
+						LastSwiftMigration = 0900;
 						ProvisioningStyle = Automatic;
 					};
 				};
@@ -878,7 +884,8 @@
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/../Frameworks";
 				PRODUCT_BUNDLE_IDENTIFIER = com.fespinozacast.LinkedIdeas;
 				PRODUCT_NAME = "$(TARGET_NAME)";
-				SWIFT_VERSION = 3.0;
+				SWIFT_SWIFT3_OBJC_INFERENCE = Off;
+				SWIFT_VERSION = 4.0;
 			};
 			name = Debug;
 		};
@@ -892,7 +899,8 @@
 				PRODUCT_BUNDLE_IDENTIFIER = com.fespinozacast.LinkedIdeas;
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SWIFT_OPTIMIZATION_LEVEL = "-Owholemodule";
-				SWIFT_VERSION = 3.0;
+				SWIFT_SWIFT3_OBJC_INFERENCE = Off;
+				SWIFT_VERSION = 4.0;
 			};
 			name = Release;
 		};
@@ -907,7 +915,8 @@
 				PRODUCT_BUNDLE_IDENTIFIER = com.fespinozacast.LinkedIdeasTests;
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SWIFT_OPTIMIZATION_LEVEL = "-Onone";
-				SWIFT_VERSION = 3.0;
+				SWIFT_SWIFT3_OBJC_INFERENCE = Off;
+				SWIFT_VERSION = 4.0;
 				TEST_HOST = "$(BUILT_PRODUCTS_DIR)/LinkedIdeas.app/Contents/MacOS/LinkedIdeas";
 			};
 			name = Debug;
@@ -923,7 +932,8 @@
 				PRODUCT_BUNDLE_IDENTIFIER = com.fespinozacast.LinkedIdeasTests;
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SWIFT_OPTIMIZATION_LEVEL = "-Owholemodule";
-				SWIFT_VERSION = 3.0;
+				SWIFT_SWIFT3_OBJC_INFERENCE = Off;
+				SWIFT_VERSION = 4.0;
 				TEST_HOST = "$(BUILT_PRODUCTS_DIR)/LinkedIdeas.app/Contents/MacOS/LinkedIdeas";
 			};
 			name = Release;
@@ -945,7 +955,8 @@
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SKIP_INSTALL = YES;
 				SWIFT_OPTIMIZATION_LEVEL = "-Onone";
-				SWIFT_VERSION = 3.0;
+				SWIFT_SWIFT3_OBJC_INFERENCE = Off;
+				SWIFT_VERSION = 4.0;
 				WRAPPER_EXTENSION = qlgenerator;
 			};
 			name = Debug;
@@ -966,7 +977,8 @@
 				PRODUCT_BUNDLE_IDENTIFIER = com.fespinozacast.QLLinkedIdeas;
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SKIP_INSTALL = YES;
-				SWIFT_VERSION = 3.0;
+				SWIFT_SWIFT3_OBJC_INFERENCE = Off;
+				SWIFT_VERSION = 4.0;
 				WRAPPER_EXTENSION = qlgenerator;
 			};
 			name = Release;

LinkedIdeas/CanvasView.swift
@@ -53,7 +53,7 @@ public class CanvasView: NSView {
 
   func drawBackground(_ dirtyRect: NSRect) {
     NSColor.white.set()
-    NSRectFill(dirtyRect)
+    dirtyRect.fill()
   }
 
   func drawSelectionRect() {

LinkedIdeas/CanvasViewController.swift
@@ -153,7 +153,7 @@ class CanvasViewController: NSViewController {
   }
 
   func isPressingShift(event: NSEvent) -> Bool {
-    return event.modifierFlags.contains(.shift)
+    return event.modifierFlags.contains(NSEvent.ModifierFlags.shift)
   }
 
   // MARK: - current State data helpers

LinkedIdeas/Concept.swift
@@ -22,7 +22,7 @@ public class Concept: NSObject, NSCoding, Element, VisualElement, AttributedStri
   var identifier: String
 
   // MARK: - NSAttributedStringElement
-  dynamic public var attributedStringValue: NSAttributedString
+  @objc dynamic public var attributedStringValue: NSAttributedString
   public var stringValue: String { return attributedStringValue.string }
 
   // visual element

LinkedIdeas/Document.swift
@@ -52,16 +52,16 @@ public class Document: NSDocument {
   private var KVOContext: Int = 0
 
   override public func makeWindowControllers() {
-    let storyboard = NSStoryboard(name: "Main", bundle: nil)
+    let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
 
     if let windowController = storyboard.instantiateController(
-                                withIdentifier: "Document Window Controller"
+                                withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "Document Window Controller")
                               ) as? WindowController {
       self.addWindowController(windowController)
     }
   }
 
-  override public class func autosavesInPlace() -> Bool {
+  override public class var autosavesInPlace: Bool {
     return true
   }
 
@@ -144,7 +144,7 @@ public class Document: NSDocument {
 }
 
 extension Document: LinkedIdeasDocument {
-  func save(concept: Concept) {
+  @objc func save(concept: Concept) {
     concepts.append(concept)
     undoManager?.registerUndo(
       withTarget: self,
@@ -153,7 +153,7 @@ extension Document: LinkedIdeasDocument {
     observer?.documentChanged(withElement: concept)
   }
 
-  func remove(concept: Concept) {
+  @objc func remove(concept: Concept) {
     concepts.remove(at: concepts.index(of: concept)!)
     undoManager?.registerUndo(
       withTarget: self,
@@ -162,7 +162,7 @@ extension Document: LinkedIdeasDocument {
     observer?.documentChanged(withElement: concept)
   }
 
-  func save(link: Link) {
+  @objc func save(link: Link) {
     links.append(link)
     undoManager?.registerUndo(
       withTarget: self,
@@ -171,7 +171,7 @@ extension Document: LinkedIdeasDocument {
     observer?.documentChanged(withElement: link)
   }
 
-  func remove(link: Link) {
+  @objc func remove(link: Link) {
     links.remove(at: links.index(of: link)!)
     undoManager?.registerUndo(
       withTarget: self,

LinkedIdeas/DrawableConcept.swift
@@ -22,7 +22,7 @@ public struct DrawableConcept: DrawableElement {
 
   func drawBackground() {
     NSColor.white.set()
-    NSRectFill(drawingBounds)
+    drawingBounds.fill()
   }
 
   func drawConceptText() {

LinkedIdeas/DrawableElement.swift
@@ -78,7 +78,7 @@ extension DrawableElement {
   }
 
   func drawFullRect(_ rect: NSRect) {
-    NSRectFill(rect)
+    rect.fill()
     drawBorderForRect(rect)
     drawCenteredDotAtPoint(rect.origin, color: NSColor.red)
     drawCenteredDotAtPoint(rect.center)

commit e2ce48afadcf3e868646d9fa4fd270822bcca27c

Fix: copy/paste meny actions.
the problem was the lacking `@objc` points.

fixes the problem introduced in: #70

because of the limited inference of @objc calls.

LinkedIdeas/CanvasViewController+NSPasteboard.swift
@@ -11,7 +11,7 @@ import Cocoa
 extension CanvasViewController {
   // MARK: - Pasteboard
 
-  private func writeToPasteboard(pasteboard: NSPasteboard) {
+  fileprivate func writeToPasteboard(pasteboard: NSPasteboard) {
     guard let elements = selectedElements() else {
       return
     }
@@ -20,7 +20,7 @@ extension CanvasViewController {
     pasteboard.writeObjects(elements.map { $0.attributedStringValue })
   }
 
-  private func readFromPasteboard(pasteboard: NSPasteboard) {
+  fileprivate func readFromPasteboard(pasteboard: NSPasteboard) {
     let rawObjects = pasteboard.readObjects(
       forClasses: [NSAttributedString.self], options: [:]
     ) as? [NSAttributedString]
@@ -32,7 +32,7 @@ extension CanvasViewController {
     pasteConcepts(fromAttributedStrings: objects)
   }
 
-  private func readFromPasteboardAsPlainText(pasteboard: NSPasteboard) {
+  fileprivate func readFromPasteboardAsPlainText(pasteboard: NSPasteboard) {
     let rawObjects = pasteboard.readObjects(forClasses: [NSString.self], options: [:]) as? [String]
 
     guard let objects = rawObjects, objects.count != 0 else {
@@ -42,7 +42,7 @@ extension CanvasViewController {
     pasteConcepts(fromAttributedStrings: objects.map { NSAttributedString(string: $0) })
   }
 
-  private func pasteConcepts(fromAttributedStrings attributedStrings: [NSAttributedString]) {
+  fileprivate func pasteConcepts(fromAttributedStrings attributedStrings: [NSAttributedString]) {
     safeTransiton {
       try stateManager.toCanvasWaiting()
     }
@@ -71,7 +71,7 @@ extension CanvasViewController {
     }
   }
 
-  private func createConceptFromPasteboard(attributedString: NSAttributedString, index: Int) -> Concept? {
+  fileprivate func createConceptFromPasteboard(attributedString: NSAttributedString, index: Int) -> Concept? {
     let newConceptPadding: Int = 15
     let newPoint = NSPoint(
       x: CGFloat(200 + (newConceptPadding*index)),
@@ -82,7 +82,7 @@ extension CanvasViewController {
 
   // MARK: - Interface actions
 
-  func cut(_ sender: Any?) {
+  @objc func cut(_ sender: Any?) {
     writeToPasteboard(pasteboard: NSPasteboard.general)
     if let elements = selectedElements() {
       safeTransiton {
@@ -91,15 +91,15 @@ extension CanvasViewController {
     }
   }
 
-  func copy(_ sender: Any?) {
+  @objc func copy(_ sender: Any?) {
     writeToPasteboard(pasteboard: NSPasteboard.general)
   }
 
-  func paste(_ sender: Any?) {
+  @objc func paste(_ sender: Any?) {
     readFromPasteboard(pasteboard: NSPasteboard.general)
   }
 
-  func pasteAsPlainText(_ sender: Any?) {
+  @objc func pasteAsPlainText(_ sender: Any?) {
     readFromPasteboardAsPlainText(pasteboard: NSPasteboard.general)
   }
 }