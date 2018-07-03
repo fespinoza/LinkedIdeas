22ba921cf9f4363672e735907941841c93594a35
Felipe Espinoza <fespinozacast@gmail.com>
2018-07-03 21:39:58 +0200

    Fix iOS test target
    added a dummy test to make sure everything compiles

---
+++ LinkedIdeas-iOS-Tests/LinkedIdeas_iOS_Tests.swift
@@ -0,0 +1,15 @@
+//
+//  LinkedIdeas_iOS_Tests.swift
+//  LinkedIdeas-iOS-Tests
+//
+//  Created by Felipe Espinoza on 03/07/2018.
+//  Copyright © 2018 Felipe Espinoza Dev. All rights reserved.
+//
+
+import XCTest
+
+class LinkedIdeas_iOS_Tests: XCTestCase {
+  func testExample() {
+    XCTAssert(1 == 1)
+  }
+}
